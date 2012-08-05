427922ea7a6d8332ce82645e8fe156dc6e9d570e
speechkey <gremoz@gmail.com>
2012-08-05 21:30:28 +0200

    New feature: Use current dictation language icon as a menu bar image

--- DictationSwitcher/DictationSwitcherAppDelegate.m
+++ DictationSwitcher/DictationSwitcherAppDelegate.m
@@ -22,6 +22,7 @@
                                                              [NSNumber numberWithBool:TRUE], @"fr-FR",
                                                              [NSNumber numberWithBool:TRUE], @"de-DE",
                                                              [NSNumber numberWithBool:TRUE], @"ja-JP",
+                                                             [NSNumber numberWithBool:TRUE], @"showIcon",
                                                              [NSNumber numberWithBool:FALSE], @"openAtLogin",
                                                              nil]];
     defaults = [[NSUserDefaults alloc] init];
@@ -29,7 +30,6 @@
     // Is dictation activated?
     
     [defaults addSuiteNamed:@"com.apple.assistant.support"];
-    
     if ([defaults boolForKey:@"Dictation Enabled"]) {
         dictationDisabled=FALSE;
         [dictationOnOff setTitle:NSLocalizedString(@"Turn dictation off", nil)];
@@ -42,13 +42,6 @@
     
     dictationSwitcherItem = [[NSStatusBar systemStatusBar] statusItemWithLength:22];
     [dictationSwitcherItem setMenu:dictationSwitcherMenu];
-    if (!dictationDisabled) {
-        [dictationSwitcherItem setImage:[[NSBundle mainBundle] imageForResource:@"statusicon"]];
-        [dictationSwitcherItem setAlternateImage:[[NSBundle mainBundle] imageForResource:@"statusicon-alt"]];
-    } else {
-        [dictationSwitcherItem setImage:[[NSBundle mainBundle] imageForResource:@"statusicon-off"]];
-        [dictationSwitcherItem setAlternateImage:[[NSBundle mainBundle] imageForResource:@"statusicon-off-alt"]];
-    }
 
     [dictationSwitcherItem setHighlightMode:YES];
     [dictationSwitcherItem setToolTip:@"DictationSwitcher"];
@@ -99,40 +92,62 @@
     // Grab the current dictation language settings:
     
     [self getCurrentLanguageSettings];
+    //Set status bar icon according to settings
+    [self setStatusBarIcon];
 }
 
 -(void)getCurrentLanguageSettings {
     
     // Grab the dictation language settings in the propertly list file for the dictation preference pane:
        
-    NSString *settings=[defaults objectForKey:@"DictationIMLocaleIdentifier"];
-    
+
+    //[DictationIMLocaleIdentifier setString:[NSString stringWithFormat:@"%@", [defaults objectForKey:@"DictationIMLocaleIdentifier"]]];
+    DictationIMLocaleIdentifier = [[NSMutableString alloc] initWithFormat:@"%@", [defaults objectForKey:@"DictationIMLocaleIdentifier"]];
     // Check the appropriate language:
     
-        if ([settings isEqualToString:@"en-AU"]) {
+        if ([DictationIMLocaleIdentifier isEqualToString:@"en-AU"]) {
             previousSender=[dictationSwitcherMenu itemWithTag:1];
             [[dictationSwitcherMenu itemWithTag:1] setState:NSOnState];
         }
-        if ([settings isEqualToString:@"en-GB"]) {
+        if ([DictationIMLocaleIdentifier isEqualToString:@"en-GB"]) {
             previousSender=[dictationSwitcherMenu itemWithTag:2];
             [[dictationSwitcherMenu itemWithTag:2] setState:NSOnState];
         }
-        if ([settings isEqualToString:@"en-US"]) {
+        if ([DictationIMLocaleIdentifier isEqualToString:@"en-US"]) {
             previousSender=[dictationSwitcherMenu itemWithTag:3];
             [[dictationSwitcherMenu itemWithTag:3] setState:NSOnState];
         }
-        if ([settings isEqualToString:@"fr-FR"]) {
+        if ([DictationIMLocaleIdentifier isEqualToString:@"fr-FR"]) {
             previousSender=[dictationSwitcherMenu itemWithTag:4];
             [[dictationSwitcherMenu itemWithTag:4] setState:NSOnState];
         }
-        if ([settings isEqualToString:@"de-DE"]) {
+        if ([DictationIMLocaleIdentifier isEqualToString:@"de-DE"]) {
             previousSender=[dictationSwitcherMenu itemWithTag:5];
             [[dictationSwitcherMenu itemWithTag:5] setState:NSOnState];
         }
-        if ([settings isEqualToString:@"ja-JP"]) {
+        if ([DictationIMLocaleIdentifier isEqualToString:@"ja-JP"]) {
             previousSender=[dictationSwitcherMenu itemWithTag:6];
             [[dictationSwitcherMenu itemWithTag:6] setState:NSOnState];
         }
+}
+
+- (void)setStatusBarIcon{
+    if (!dictationDisabled) {
+        if([defaults boolForKey:@"showIcon"]){
+            [dictationSwitcherItem setImage:[[NSBundle mainBundle] imageForResource:DictationIMLocaleIdentifier]];
+            [dictationSwitcherItem setAlternateImage:[[NSBundle mainBundle] imageForResource:DictationIMLocaleIdentifier]];
+        } else {
+            [dictationSwitcherItem setImage:[[NSBundle mainBundle] imageForResource:@"statusicon"]];
+            [dictationSwitcherItem setAlternateImage:[[NSBundle mainBundle] imageForResource:@"statusicon-alt"]];
+        }
+    } else {
+        [dictationSwitcherItem setImage:[[NSBundle mainBundle] imageForResource:@"statusicon-off"]];
+        [dictationSwitcherItem setAlternateImage:[[NSBundle mainBundle] imageForResource:@"statusicon-off-alt"]];
+    }
+}
+
+-(IBAction)useDefaultStatusBarIcon:(id)sender{
+    [self setStatusBarIcon];
 }
 
 - (IBAction)switchLanguage:(id)sender {
@@ -141,32 +156,33 @@
     
     switch ([sender tag]) {
         case 1:
-            DictationIMLocaleIdentifier=@"en-AU";
+            [DictationIMLocaleIdentifier setString:@"en-AU"];
             break;
             
         case 2:
-            DictationIMLocaleIdentifier=@"en-GB";
+            [DictationIMLocaleIdentifier setString:@"en-GB"];
             break;
             
         case 3:
-            DictationIMLocaleIdentifier=@"en-US";
+            [DictationIMLocaleIdentifier setString:@"en-US"];
             break;
             
         case 4:
-            DictationIMLocaleIdentifier=@"fr-FR";
+            [DictationIMLocaleIdentifier setString:@"fr-FR"];
             break;
             
         case 5:
-            DictationIMLocaleIdentifier=@"de-DE";
+            [DictationIMLocaleIdentifier setString:@"de-DE"];
             break;
             
         case 6:
-            DictationIMLocaleIdentifier=@"ja-JP";
+            [DictationIMLocaleIdentifier setString:@"ja-JP"];
             break;
             
         default:
             break;
     }
+    [self setStatusBarIcon];
     
     // Deactivate the previous checkmark (if available) and set the checkmark:
     
@@ -211,20 +227,18 @@
     if (!dictationDisabled) {
         dictationDisabled=TRUE;
     
-        [sender setTitle:NSLocalizedString(@"Turn dictation on", nil)],
-    
-        [dictationSwitcherItem setImage:[[NSBundle mainBundle] imageForResource:@"statusicon-off"]];
-        [dictationSwitcherItem setAlternateImage:[[NSBundle mainBundle] imageForResource:@"statusicon-off-alt"]];
-    
+        [sender setTitle:NSLocalizedString(@"Turn dictation on", nil)];
+
+        [self setStatusBarIcon];
+
         [defaults setPersistentDomain:[NSDictionary dictionaryWithObject:[NSNumber numberWithBool:NO] forKey:@"Dictation Enabled"]
                       forName:@"com.apple.assistant.support"];
     } else {
         dictationDisabled=FALSE;
 
-        [sender setTitle:NSLocalizedString(@"Turn dictation off", nil)],
-    
-        [dictationSwitcherItem setImage:[[NSBundle mainBundle] imageForResource:@"statusicon"]];
-        [dictationSwitcherItem setAlternateImage:[[NSBundle mainBundle] imageForResource:@"statusicon-alt"]];
+        [sender setTitle:NSLocalizedString(@"Turn dictation off", nil)];
+
+        [self setStatusBarIcon];
 
         [defaults setPersistentDomain:[NSDictionary dictionaryWithObject:[NSNumber numberWithBool:YES] forKey:@"Dictation Enabled"]
                           forName:@"com.apple.assistant.support"];
